9529d2225b2d7c956dd3813301bf7e0ca497d7f0
Squibs <z.r.holman+github@gmail.com>
2018-04-24 05:05:42 -0600

    My react-redux boilerplate to start off the project. Initial commit.

---
+++ src/components/app.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
+
+/* eslint-disable react/prefer-stateless-function */
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Boilerplate
+      </div>
+    );
+  }
+}
+
+export default hot(module)(App);
